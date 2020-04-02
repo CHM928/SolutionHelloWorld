f3795b7f9bcde0e419ee7c81e570bdcadba90709
浩淼 程 <1209876685@qq.com>
2020-04-02 19:25:00 +0800

    up

---
+++ WebApplication1/PrintNumberPyramid.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class PrintNumberPyramid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder result = new StringBuilder();
+            int lineMax = int.Parse(txtNumber.Text);
+            if (lineMax >= 10 || lineMax < 0)
+                return;
+            else
+            {
+                for (int currentLine = 1; currentLine <= lineMax; currentLine++)
+                {
+                    for (int j = 1; j <= (lineMax - currentLine); j++)
+                        result.Append("&nbsp;&nbsp;");
+                    for (int star = 1; star <= currentLine; star++)
+                        result.Append(star);
+                    for (int next = currentLine - 1; next > 0; next = next - 1)
+                        result.Append(next);
+                    result.Append("<br>");
+
+                }
+                Response.Write(result);
+            }
+
+
+        }
+    }
+}
--- WebApplication1/getDay.aspx.cs
+++ WebApplication1/getDay.aspx.cs
@@ -16,17 +16,17 @@ namespace WebApplication1
 
         protected void day_Click(object sender, EventArgs e)
         {
-            double num1 = double.Parse(year.Text);
-            double num2 = double.Parse(month.Text);
-            double sum = double.Parse(date.Text);
+            int num1 = int.Parse(year.Text);
+            int num2 = int.Parse(month.Text);
+            int sum = int.Parse(date.Text);
             int[] yearnum = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-            if (num1%4==0)
+            if ( num1 % 4 ==0 && num1 % 100 !=0 || num1 % 400 == 0)
             {
                 yearnum[1] = 29;
-            }
+            }            
             for (int i = 0; i < num2-1; i++)
             {
-                sum = sum + yearnum[i];
+                sum += yearnum[i];
             }
             Response.Write("这个日期是该年的第" + sum + "天");
 
